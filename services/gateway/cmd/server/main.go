4f7777f6308c6455291a7e7b032e8f222eadced8
Shanu Chhetri <shanu.chhetri529@gmail.com>
2026-08-05 12:29:23 +0530

    add Gin server with health endpoints and logging middleware

---
+++ services/gateway/cmd/server/main.go
@@ -0,0 +1,37 @@
+package main
+
+// import "fmt"
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"sync/atomic"
+    "github.com/Shanu529/atla-voice-agent/services/gateway/internal/middlewares"
+)
+
+func main() {
+	r := gin.New()
+
+	var isReady atomic.Bool
+    isReady.Store(true)
+
+    r.Use(middlewares.Logger())
+    r.Use(gin.Recovery())
+
+	// Liveness: is the process alive?
+	r.GET("/healthy", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "alive"})
+	})
+
+	// Readiness: can we serve traffic?
+	r.GET("/readyz", func(c *gin.Context) {
+		if !isReady.Load() {
+			c.JSON(http.StatusServiceUnavailable, gin.H{"status": "Not Ready"})
+
+		} else {
+			c.JSON(http.StatusOK, gin.H{"status": "Ready"})
+		}
+	})
+
+	r.Run(":8080")
+}
---
+++ services/gateway/internal/middlewares/logger.go
@@ -0,0 +1,19 @@
+package middlewares
+
+import (
+	"log";
+	"time";
+	"github.com/gin-gonic/gin";
+)
+
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context){
+		start := time.Now();
+		log.Println("Request received: ", c.Request.Method, " ", c.Request.URL.Path);
+
+		c.Next();
+
+		log.Println("time", time.Since(start))
+	}
+}
+
