2a951aa6946291d4da182245564882a7a7d30139
Shanu Chhetri <shanu.chhetri529@gmail.com>
2026-08-06 19:13:53 +0530

    ..

--- services/gateway/internal/middlewares/logger.go
+++ services/gateway/internal/middlewares/logger.go
@@ -9,11 +9,13 @@ import (
 func Logger() gin.HandlerFunc {
 	return func(c *gin.Context){
 		start := time.Now();
+		
 		log.Println("Request received: ", c.Request.Method, " ", c.Request.URL.Path);
 
 		c.Next();
-
+		
 		log.Println("time", time.Since(start))
+
 	}
 }
 
