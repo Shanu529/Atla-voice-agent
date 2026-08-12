cf4427bf2d78c5e044034e818aaaecdaf0f39f5d
Shanu Chhetri <shanu.chhetri529@gmail.com>
2026-08-12 17:23:13 +0530

    refactor main.go to integrate agent handler and remove unused endpoints; update .gitignore

--- services/gateway/cmd/server/main.go
+++ services/gateway/cmd/server/main.go
@@ -1,63 +1,35 @@
 package main
 
 import (
-	"io"
-	"net/http"
-	"sync/atomic"
-
+	"github.com/Shanu529/atla-voice-agent/services/gateway/internal/clients"
+	"github.com/Shanu529/atla-voice-agent/services/gateway/internal/handlers"
 	"github.com/Shanu529/atla-voice-agent/services/gateway/internal/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.New()
 
-	var isReady atomic.Bool
-	isReady.Store(true)
+	// its gin router instance
+	r := gin.New()
 
 	r.Use(gin.Recovery())
 	r.Use(middlewares.Logger())
 
-	// Liveness: is the process alive or not?
-	r.GET("/healthy", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"status": "alive"})
-	})
 
-	// Readiness: can we serve traffic?
-	r.GET("/readyz", func(c *gin.Context) {
-		if !isReady.Load() {
-			c.JSON(http.StatusServiceUnavailable, gin.H{"status": "Not Ready"})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"status": "Ready"})
-		}
-	})
+	// create agent client to communicate with agent service
+	agentClient := clients.NewAgentClient(
+		"http://localhost:8081",
+	)
 
-	// Reverse proxy endpoint to downstream agent
-	r.GET("/api/agent/hello", func(c *gin.Context) {
-		resp, err := http.Get("http://localhost:8081/hello")
-		if err != nil {
-			c.JSON(http.StatusBadGateway, gin.H{
-				"error": "Agent service unavailable",
-			})
-			return
-		}
-		
-		defer resp.Body.Close()
+	// create agent handler to hande requests from gateway to agent service
+	agentHandler := handlers.NewAgentHandler(
+		agentClient,
+	)
 
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			c.JSON(http.StatusBadGateway, gin.H{
-				"error": "Failed to read agent response",
-			})
-			return
-		}
-
-		c.Data(
-			resp.StatusCode,
-			"application/json",
-			body,
-		)
-	})
-
+	r.GET(
+		"/api/agent/hello",
+		agentHandler.Hello,
+	)
+	
 	r.Run(":8080")
 }
