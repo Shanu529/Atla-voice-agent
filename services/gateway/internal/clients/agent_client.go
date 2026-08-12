500638cf184bde6a25d27d7390744e4c89341560
Shanu Chhetri <shanu.chhetri529@gmail.com>
2026-08-12 17:24:02 +0530

    created agent_client

--- services/gateway/internal/clients/agent_client.go
+++ services/gateway/internal/clients/agent_client.go
@@ -1,1 +1,55 @@
 package clients
+package clients
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type AgentClient struct {
+	baseURL    string
+	httpClient *http.Client
+}
+
+func NewAgentClient(baseURL string) *AgentClient {
+	return &AgentClient{
+		baseURL: baseURL,
+
+		// We will configure timeout properly later.
+		httpClient: &http.Client{},
+	}
+}
+
+func (a *AgentClient) Hello(ctx context.Context) ([]byte, int, error) {
+
+	url := fmt.Sprintf("%s/hello", a.baseURL)
+
+	req, err := http.NewRequestWithContext(
+		ctx,
+		http.MethodGet,
+		url,
+		nil,
+	)
+
+	if err != nil {
+		return nil, 0, err
+	}
+
+	resp, err := a.httpClient.Do(req)
+
+	if err != nil {
+		return nil, 0, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return body, resp.StatusCode, nil
+}
