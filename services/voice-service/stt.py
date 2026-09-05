75d6205e0903d310dec33e9ffe97aa2d19846cba
Shanu Chhetri <shanu.chhetri529@gmail.com>
2026-09-05 15:17:52 +0530

    feat(ai-engine): add audio transcription functionality

--- services/voice-service/stt.py
+++ services/voice-service/stt.py
@@ -2,6 +2,8 @@
 
 # pip install scipy
 
+from http import client
+
 import sounddevice as sd
 from scipy.io.wavfile import write
 
@@ -28,5 +30,22 @@ def record_audio(filename="input.wav"):
     print(f"Audio saved: {filename}")
 
 
+
+def transcribe_audio(filename="input.wav"):
+    with open(filename, "rb") as audio_file:
+        transcript = client.audio.transcriptions.create(
+            model="whisper-large-v3-turbo", file=audio_file
+        )
+    return transcript.text
+
+
+
+
+
 if __name__ == "__main__":
     record_audio()
+
+    text = transcribe_audio()
+
+    print(f"Transcribed text: {text}")
+
